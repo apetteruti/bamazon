cbcb67cf1ba8af90dd786381b49a40e24e549436
ALP <alpetteruti@gmail.com>
2019-01-13 20:23:52 -0500

    Minimum requirements met

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -115,19 +115,20 @@ function buyProduct() {
                   }
                   //create variable to hold the price amount and then report it
                 ); 
+              }
 
+              else{
+                console.log("Unfortunately, due to limited supply, your order cannot be completed at this time.")
+                connection.end();
               }
             }
 
             // else{
-            //   console.log ("The item number you entered was not found. Please try again.")
+            //   console.log ("The item number you entered was not found. Please try again.")};
 
              
-
-                
                 // connection.end();
               
-          
           };
           // }
 
@@ -137,20 +138,3 @@ function buyProduct() {
 }
 
 
-
-
-
-
-
-
-
-// function buyAmount()
-//    * The second message should ask how many units of the product they would like to buy.
-
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//    * This means updating the SQL database to reflect the remaining quantity.
-//    * Once the update goes through, show the customer the total cost of their purchase.
