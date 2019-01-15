7d9abdc2283075a245314e0ad4080874c91b9d6f
ALP <alpetteruti@gmail.com>
2019-01-14 19:15:27 -0500

    Manager function only works on two commands

--- bamazonManager.js
+++ bamazonManager.js
@@ -42,107 +42,148 @@ function productsSale() {
         }
         console.log(table.toString());
 
+        connection.end();
     });
-    optionsMenu();
 };
 
-function lowInventory(){
-    connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
-        for (var i = 0; i < results.length; i++) {
-            // console.log(results[i].item_id);
-            // console.log(results[i].product_name);
-            // console.log(results[i].price);
-            table.push(
-                [results[i].item_id, results[i].product_name, results[i].price, results[i].stock_quantity]
-            );
-        }
-        console.log(table.toString());
-
-    });
-    optionsMenu();
+//checks for low inventory (5 or less)
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity<=5",
+        function (err, results) {
+            if (err) throw err;
+            for (var i = 0; i < results.length; i++) {
+                table.push(
+                    [results[i].item_id, results[i].product_name, results[i].price, results[i].stock_quantity]
+                );
+            }
+            console.log(table.toString());
+            connection.end();
+            optionsMenu();
+        });
 }
 
-//productsSale function that will list the products for sale
+//add inventory will add to the stock_quantity
+function addInventory() {
+    inquirer.prompt([{
+                name: "input_product",
+                type: "input",
+                message: "Enter the item number to which you would like to add stock:",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                },
+            },
+            {
+                //The user is asked the amount
+                name: "item_amount",
+                type: "input", 
+                message: "How many would you like to add to stock?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+
+        ]).then(function (answer) {
+
+                // connection.query ("SELECT stock_quantity FROM products WHERE item_id = ?", 
+                //  function(error, results){
+                //      if(error) throw err;
+                //      var newStock = answer.item_amount + stock_quantity;
+                //      console.log (newStock);
+                     
+                     connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?,",
+                     [(stock_quantity + answer.item_amount), answer.input_product],
+                     
+                     function (error, results) {
+                         if (error) throw err;
+                         console.log (answer.input_product + "has been updated and how has " + stock_quantity);
+                         
+                        });
+                    }
+                    )
+
+                // })
+            }
 
 
+            var optionsMenu = function () {
 
-var optionsMenu = function () {
+                inquirer.prompt([{
 
-        inquirer.prompt([{
+                    name: "options",
+                    type: "list",
+                    choices: ["Products for Sale", "Low Inventory", "Add to Inventory", "Add New Product"],
+                    message: "What would you like to do?"
 
-            name: "options",
-            type: "list",
-            choices: ["Products for Sale", "Low Inventory", "Add to Inventory", "Add New Product"],
-            message: "What would you like to do?"
-
-        }]).then(function (answer) {
-                switch (answer.options) {
-                    case "Products for Sale":
-                        productsSale();
-                        break;
-                    case "Low Inventory":
-                        lowInventory();
-                        break;
-                    case "Add to Inventory":
-                        addInventory();
+                }]).then(function (answer) {
+                    switch (answer.options) {
+                        case "Products for Sale":
+                            productsSale();
+                            break;
+                        case "Low Inventory":
+                            lowInventory();
+                            break;
+                        case "Add to Inventory":
+                            addInventory();
 
                         case "Add new Product":
-                        addProduct();
-  
-                }
+                            addProduct();
 
-            })
-    }
+                    }
 
-
-    connection.connect(function (err) {
-        if (err) throw err;
-        console.log("connected as id " + connection.threadId);
-        optionsMenu();
-    });
-
-//         ### Challenge #2: Manager View (Next Level)
+                })
+            }
 
 
+            connection.connect(function (err) {
+                if (err) throw err;
+                console.log("connected as id " + connection.threadId);
+                optionsMenu();
+            });
 
-// * Create a new Node application called `bamazonManager.js`. Running this application will:
+            //         ### Challenge #2: Manager View (Next Level)
 
 
 
-//   * List a set of menu options:
+            // * Create a new Node application called `bamazonManager.js`. Running this application will:
 
 
 
-//     * View Products for Sale
-
-    
-
-//     * View Low Inventory
-
-    
-
-//     * Add to Inventory
-
-    
-
-//     * Add New Product
+            //   * List a set of menu options:
 
 
 
-//   * If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
+            //     * View Products for Sale
 
 
 
-//   * If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
+            //     * View Low Inventory
 
 
 
-//   * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+            //     * Add to Inventory
 
 
 
-//   * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
+            //     * Add New Product
 
 
 
+            //   * If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
+
+
+
+            //   * If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
+
+
+
+            //   * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+
+
+
+            //   * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
